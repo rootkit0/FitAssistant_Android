908c9ac77534c1cfe703f72f875bab531bda7d90
Albert <asm28@alumnes.udl.cat>
2021-04-25 14:21:27 +0200

    Filled RoutineListAdapter

--- app/src/main/java/com/example/fitassistant/RoutineListAdapter.java
+++ app/src/main/java/com/example/fitassistant/RoutineListAdapter.java
@@ -1,4 +1,69 @@
 package com.example.fitassistant;
 
-public class RoutineListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RoutineListAdapter extends RecyclerView.Adapter<RoutineListAdapter.ViewHolder> {
+
+    private List<DietElement> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public RoutineListAdapter(List<DietElement> itemList, Context context) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = itemList;
+        this.context = context;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    @Override
+    public RoutineListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.diet_list_element, null);
+        return new RoutineListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final RoutineListAdapter.ViewHolder holder, final int position) {
+        holder.bindData(mData.get(position));
+    }
+
+    public void setItems(List<DietElement> items) {
+        mData = items;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iconImage;
+        TextView name;
+        TextView description;
+        ImageView isVegan;
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            iconImage = itemView.findViewById(R.id.diet_image);
+            name = itemView.findViewById(R.id.diet_name);
+            description = itemView.findViewById(R.id.diet_description);
+            isVegan = itemView.findViewById(R.id.isVegan);
+
+        }
+
+        public void bindData(final DietElement item) {
+            iconImage.setImageResource(item.image);
+            name.setText(item.name);
+            description.setText(item.description);
+            if (!item.isVegan) isVegan.setColorFilter(R.color.material_on_primary_disabled);
+        }
+    }
 }
