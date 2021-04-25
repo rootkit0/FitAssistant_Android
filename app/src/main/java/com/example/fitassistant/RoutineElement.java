c1064ba89525126469c3d0df415cc9c565ad1089
Albert <asm28@alumnes.udl.cat>
2021-04-25 14:19:19 +0200

    Filled RoutineElement

--- app/src/main/java/com/example/fitassistant/RoutineElement.java
+++ app/src/main/java/com/example/fitassistant/RoutineElement.java
@@ -1,4 +1,39 @@
 package com.example.fitassistant;
 
 public class RoutineElement {
+    public String name;
+    public int image;
+    public String description;
+
+
+    public RoutineElement(String name, int image, String description) {
+        this.name = name;
+        this.image = image;
+        this.description = description;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
