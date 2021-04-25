7e0f9cd97d0b28e27fa347a87d7f685fbb65c3a3
Albert <asm28@alumnes.udl.cat>
2021-04-25 13:29:00 +0200

    Finished diets fragment with examples

--- app/src/main/java/com/example/fitassistant/DietsFragment.java
+++ app/src/main/java/com/example/fitassistant/DietsFragment.java
@@ -2,11 +2,19 @@ package com.example.fitassistant;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,44 +23,21 @@ import android.view.ViewGroup;
  */
 public class DietsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    List<DietElement> diets;
 
     public DietsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DietsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DietsFragment newInstance(String param1, String param2) {
+    public static DietsFragment newInstance() {
         DietsFragment fragment = new DietsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        createExamples();
     }
 
     @Override
@@ -60,5 +45,31 @@ public class DietsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_diets, container, false);
+    }
+
+    private void createExamples() {
+        diets = new ArrayList<DietElement>();
+        diets.add(new DietElement("Dieta Hipercalòrica vegana", R.drawable.ic_food,
+                true, "Feta per guanyar massa muscular"));
+        diets.add(new DietElement("Dieta Hipercalòrica", R.drawable.ic_food,
+                false, ""));
+        diets.add(new DietElement("Dieta manteniment", R.drawable.ic_food,
+                false, "Dieta per mantenir la massa muscular"));
+        diets.add(new DietElement("Dieta hipocalòrica vegana", R.drawable.ic_food,
+                true, "Ideal per aprimar"));
+        diets.add(new DietElement("Dieta Hipocalòrica", R.drawable.ic_food,
+                false, "Ideal per aprimar"));
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListAdapter listAdapter = new ListAdapter(diets, this.getContext());
+        RecyclerView recyclerView = view.findViewById(R.id.diet_list_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(listAdapter);
+
     }
 }
