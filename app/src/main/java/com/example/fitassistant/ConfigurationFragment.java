7abbbc3e8551699b4637e9f5cd06e83c8b802107
Albert <asm28@alumnes.udl.cat>
2021-04-25 18:10:33 +0200

    modifying configuration with sharedpreferences

--- app/src/main/java/com/example/fitassistant/ConfigurationFragment.java
+++ app/src/main/java/com/example/fitassistant/ConfigurationFragment.java
@@ -1,58 +1,47 @@
 package com.example.fitassistant;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConfigurationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
 public class ConfigurationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ConfigurationFragment() {
-        // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfigurationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConfigurationFragment newInstance(String param1, String param2) {
-        ConfigurationFragment fragment = new ConfigurationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ConfigurationFragment newInstance() {
+        return new ConfigurationFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    private void loadPreferences() {
+        SharedPreferences preferences = Objects.requireNonNull(getActivity())
+                .getSharedPreferences("configuration", Context.MODE_PRIVATE);
+
+        String user = preferences.getString("user", "default_user");
+        String password = preferences.getString("password", "1234");
+        int imageView = preferences.getInt("image_id", R.drawable.ic_userconfig);
+        String mail = preferences.getString("mail", "");
+        int phoneNumber = preferences.getInt("phone_number", 600000000);
+
+
+
+
+
     }
 
     @Override
@@ -60,5 +49,15 @@ public class ConfigurationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_configuration, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        loadPreferences();
+
+
     }
 }
